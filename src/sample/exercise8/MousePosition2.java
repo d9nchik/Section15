a9817c0d8687fca135022a5416fc66b61917e08c
Danylo Halaiko <dan.galaiko@gmail.com>
2020-04-15 17:21:38 +0300

    Exercise8 has been done. I created two programs, that act different on how you press mouse left button.

---
+++ src/sample/exercise8/MousePosition2.java
@@ -0,0 +1,36 @@
+package sample.exercise8;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class MousePosition2 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Pane pane = new Pane();
+        Text text = new Text();
+
+        Scene scene = new Scene(pane, 300, 100);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Exercise 8");
+        primaryStage.show();
+
+        scene.setOnMouseClicked(e -> showCoordinates(pane, text, e));
+    }
+
+    private void showCoordinates(Pane pane, Text text, MouseEvent e) {
+        text.setText("(" + e.getX() + ", " + e.getY() + ")");
+        text.setX(e.getX());
+        text.setY(e.getY());
+        if (!pane.getChildren().contains(text))
+            pane.getChildren().add(text);
+    }
+}
