ce1dc670d0939544a066499ec58f0826399a8c0a
Danylo Halaiko <dan.galaiko@gmail.com>
2020-04-16 13:11:33 +0300

    Exercise12 has been done. Text near mouse point shows, weather mouse point inside circle or not.

---
+++ src/sample/exercise12/IsMouseInsideCircle.java
@@ -0,0 +1,41 @@
+package sample.exercise12;
+
+import javafx.application.Application;
+import javafx.geometry.Point2D;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class IsMouseInsideCircle extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Circle circle = new Circle(100, 60, 50);
+        circle.setStroke(Color.BLACK);
+        circle.setFill(Color.WHITE);
+        Text text = new Text();
+
+        Pane pane = new Pane(circle, text);
+
+        Scene scene = new Scene(pane, 300, 150);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Exercise 12");
+        primaryStage.show();
+
+        scene.setOnMouseMoved(e -> {
+            text.setX(e.getX());
+            text.setY(e.getY());
+            if (circle.contains(new Point2D(e.getX(), e.getY())))
+                text.setText("Mouse point is inside the circle");
+            else
+                text.setText("Mouse point is not inside circle");
+        });
+    }
+}
